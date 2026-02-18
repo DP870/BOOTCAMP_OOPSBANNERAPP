ebd99d4fec445c49e0489b15647a8d054c16a9af
DP870 <projectdhruv0@gmail.com>
2026-02-18 15:11:02 +0530

    Merging UC2 with dev

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 * OOPSBannerApp UC1-OOPS Banner Display Application
 * This Class demonstrates a simple Java program that uses the 
@@ -12,3 +13,26 @@ class OOPSBannerApp{
         System.out.print("OOPS");
     }
 }
+=======
+/** 
+* OOPSBANNERAPP UC1 - OOPS BANNER Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console with the help of "*".
+*
+*@author Developer
+*@version 2.0
+*/
+public class OOPSBannerApp{
+	public static void main (String[] args){
+	
+		System.out.println("   ***        ***      ******  *****"    );
+		System.out.println(" **   **    **    **   **  **  *   "  );
+		System.out.println("**      **  **      ** **  **  *"     );
+		System.out.println("**      **  **      ** ******  *****"   );
+		System.out.println("**      **  **      ** **          *      "       );
+		System.out.println(" **   **    **    **   **          *"      );
+		System.out.println("   ***         ***     **      *****"     );
+	}
+}
+>>>>>>> feature/UC2-PrintOOPS
