39dd7e7952905cd925d6734ecdba4d5d2cfee7f2
DP870 <projectdhruv0@gmail.com>
2026-02-18 14:49:23 +0530

    Updating UC2 Branch

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/** 
+* OOPSBANNERAPP UC1 - OOPS BANNER Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+*Oriented Programming System OOPS acronym to the console.
+*
+*@author Developer
+*@version 1.0
+*/
+
+public class OOPSBannerApp{
+	public static void main (String[] args){
+	
+		System.out.println("OOPS");
+	}
+}
