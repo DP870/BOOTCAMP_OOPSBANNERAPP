a2eaac258b6b0954561cfc79d0c2ad3965165d75
DP870 <projectdhruv0@gmail.com>
2026-02-18 20:54:34 +0530

    Adding Feature 6 to the OOPSBANNERAPP program

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,63 @@
 /** 
-* OOPSBANNERAPP UC5 - OOPS BANNER Display Application
+* OOPSBANNERAPP UC6 - OOPS BANNER Display Application
 *
-* This class demonstrates a simple Java application that displays the OOPS logo stored in an
-* array and uses a for loop for printing the pattern.
+* This class demonstrates a Java application that displays the OOPS logo using "*".
+* The code makes use of different methods for each characters to do the same.
 *
 *
 <<<<<<< HEAD
 * @author Developer
-* @version 5.0
+* @version 6.0
 */
 
 public class OOPSBannerApp{
-	public static void main (String[] args){
-		String[] lines={"   ***        ***      ******   *****",
-						" **   **    **   **    **  **   *   ",
-						"**      ** **     **   **  **   *",
-						"**      ** **     **   ******   *****",
-						"**      ** **     **   **           * ",
-						" **   **   **     **   **           *",
-						"   ***        ***      **       *****"
-						};
-			
-		
-		for (String pattern:lines){
-				System.out.println(pattern);
-		}
-
 	
+	public static String[] getOPattern(){
+			return new String[]{
+				"    ***     ",
+				"  **   **   ",
+				" **     **  ",
+				" **     **  ",
+				" **    **   ",
+				" **    **   ",
+				"    ***     "
+				
+			};
 	}
+	public static String[] getPPattern(){
+			return new String[]{
+				" ****** ",
+				" **  ** ",
+				" **  ** ",
+				" ****** ",
+				" **     ",
+				" **     ",
+				" **     "
+			};
+	}
+	public static String[] getSPattern(){
+			return new String[]{
+				" *****",
+				" **",
+				" **",
+				" *****",
+				"    **",
+				"    **",
+				" *****"
+			};
+	}
+	public static void main (String[] args){
+		String[] O=getOPattern();
+		String[] P=getPPattern();
+		String[] S=getSPattern();
+		for (int i=0;i<O.length;i++){
+			System.out.println(O[i]+O[i]+P[i]+S[i]);
+		}
+		
+		
+	}
+	
+	
 }
 
 
